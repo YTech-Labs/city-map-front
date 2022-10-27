aa3a6b1c62f764d44e91ec441973a56093684070
Malik <snapfirecookie@gmail.com>
2022-10-28 00:19:41 +0500

    Google auth added

--- src/components/profile_component.vue
+++ src/components/profile_component.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="wrapper">
     <h1>Profile</h1>
+    <div class="social-auth">
+      <a :href="getGoogleUrl(from)" class="google-auth">
+        <img :src="GoogleLogo" alt="Google Logo" />
+        <span>Google</span>
+      </a>
+    </div>
   </div>
 </template>
 
-<script>
-  export default {
-    name: "Profile",
-    data() {
-    },
-    mounted() {
+<script setup>
+import GoogleLogo from '/assets/search.png';
+import { getGoogleUrl } from '../utils/getGoogleUrl';
+
+const from = '/';
+
+  // export default {
+  //   name: "Profile",
+  //   data() {
+  //     return {
+  //       google_url: getGoogleUrl(from),
+  //       google_logo: '/assets/search.png'
+  //     }
+  //   },
+  //   mounted() {
+
       // Function for dynamically changing the label of the current page
       for (let x = 0; x < document.querySelector('.active.exact-active').attributes.length; x++) {
         if (document.querySelector('.active.exact-active').attributes[x].name == 'modelvalue') {
@@ -17,10 +33,63 @@
           document.querySelector('#page-title').append(document.querySelector('.active.exact-active').attributes[x].nodeValue);
         }
       }
-    },
-  }
-</script>
+//     },
+//   }
+// </script>
 
 <style scoped>
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: Roboto, sans-serif;
+    color: #222;
+    font-size: 1.6rem;
+  }
+
+  .container {
+    background-color: #2363eb;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .social-auth {
+    max-width: 27rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .google-auth {
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 0.6rem 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .google-auth img {
+    height: 4rem;
+    margin-right: 1rem;
+  }
+  .google-auth span {
+    font-size: 1.8rem;
+  }
+
+  .google-auth:hover {
+    box-shadow: 0 1px 13px 0 rgb(0 0 0 / 15%);
+  }
 </style>
